42c0ae16b029142d0b225f7021e2a425115df5b2
nubiadev19 <nubiadlarosa19@gmail.com>
2024-08-02 20:09:32 -0500

    Actuliza el app

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let titulo=document.querySelector('h1');
 titulo.innerHTML="Hora del desafío"
 
+//Crea una función que muestre en la consola el mensaje "El botón fue clicado" 
+//iempre que se presione el botón "Console".
+function mostrarMensaje() {
+    console.log("El botón fue clicado");
+
+}
